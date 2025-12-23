5b26dc4f519625d67718ff9f74ed69486a481f3c
Kushal Rathore <kushalrathore@Kushals-MacBook-Pro-4.local>
2025-12-24 00:14:54 +0530

    Added Night filter using CLAHE

--- Computer_Vision/Night_Vision_Filter.py
+++ Computer_Vision/Night_Vision_Filter.py
@@ -12,6 +12,21 @@ while True:
     ret, frame = cap.read()
     if not ret:
         break
+    #conver to LAB
+    lab = cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+    #split into LAB = (l, a, b) channels
+    l, a, b = cv2.split(lab)
+    
+    #apply CLAHE to l-channel only
+    cl = clahe.apply(l)
+    
+    #merge enhanced l-channel with a and b channels
+    enhanced_lab = cv2.merge((cl, a, b))
+    
+    #convert back to BGR color space
+    enhanced_frame = cv2.cvtColor(enhanced_lab, cv2.COLOR_LAB2BGR)
+    #display the resulting frame
+    cv2.imshow('Night Vision Filter', enhanced_frame)
     
     # to end the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
